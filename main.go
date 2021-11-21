4534cded30ab55f8fe5760de756e2a2aadeea3fb
An Nguyen <an.nguyen1@be.com.vn>
2021-11-21 23:27:08 +0700

    init

--- main.go
+++ main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
+	b64 "encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
 )
+
+type IPInfo struct {
+	IP       string `json:"ip"`
+	Hostname string `json:"hostname"`
+	City     string `json:"city"`
+	Region   string `json:"region"`
+	Country  string `json:"country"`
+	Loc      string `json:"loc"`
+	Org      string `json:"org"`
+	Postal   string `json:"postal"`
+	Timezone string `json:"timezone"`
+	Readme   string `json:"readme"`
+	Count    int64
+}
+
+func (receiver IPInfo) string() string {
+	return fmt.Sprintf("%s:%s\n-%s-%s-%d", "ip", receiver.IP, receiver.Loc, receiver.Timezone, receiver.Count)
+}
+
+var IpInfos = []*IPInfo{}
 
 func main() {
 	port := os.Getenv("PORT")
-
+	port = "8080"
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
@@ -22,7 +44,30 @@ func main() {
 	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		if cookie, _ := c.Request.Cookie("ipinfo"); cookie != nil {
+			sDec, _ := b64.StdEncoding.DecodeString(cookie.Value)
+			fmt.Println(string(sDec))
+			ip := IPInfo{}
+			err := json.Unmarshal([]byte(sDec), &ip)
+			if err == nil {
+				flag := false
+				for _, p := range IpInfos {
+					if p.IP == ip.IP {
+						p.Count += 1
+						flag = true
+						break
+					}
+				}
+				if !flag {
+					ip.Count = 2
+					IpInfos = append(IpInfos, &ip)
+				}
+			}
+		}
+		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
+			"top":  IpInfos,
+			"last": IpInfos,
+		})
 	})
 
 	router.Run(":" + port)
